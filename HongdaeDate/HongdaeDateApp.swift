fc92f21e5c125e6d13f77ef100d9e492e1883a31
choijun0 <cjy00050005@gmail.com>
2023-02-23 17:53:14 +0900

    Initial Commit

---
+++ HongdaeDate/HongdaeDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongdaeDateApp.swift
+//  HongdaeDate
+//
+//  Created by 최준영 on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HongdaeDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
